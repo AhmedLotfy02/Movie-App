5376d67f882eb6b847e4f12dc954976a2cf2e4e2
AhmedLotfy02 <76037906+AhmedLotfy02@users.noreply.github.com>
2021-10-04 16:27:20 +0200

    test
    Fetching Posts

--- movie_app/lib/main.dart
+++ movie_app/lib/main.dart
@@ -2,8 +2,12 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:movie_app/controllers/appcontroller.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
+
+import 'models/post.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -28,6 +32,13 @@ class MyHome extends StatefulWidget {
 }
 
 class _MyHomeState extends State<MyHome> {
+  late Future<List<Post>> futurePostsData;
+  @override
+  void initState() {
+    super.initState();
+    futurePostsData = AppController.getPosts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,13 +47,52 @@ class _MyHomeState extends State<MyHome> {
         actions: [
           IconButton(
               onPressed: () async {
-                Response response = await http.get(
-                    Uri.parse('https://jsonplaceholder.typicode.com/posts'));
-                print(jsonDecode(response.body) );
+                var result = await AppController.getPosts();
+                print(result);
               },
               icon: Icon(Icons.send))
         ],
       ),
+      body: FutureBuilder<List<Post>>(
+          future: futurePostsData,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      child: Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${snapshot.data![index].title}',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '${snapshot.data![index].body}',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  });
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
     );
   }
 }
