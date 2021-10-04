867e901bafdd24d0e046769d2cb7d75dba6a1b87
AhmedLotfy02 <76037906+AhmedLotfy02@users.noreply.github.com>
2021-10-04 14:10:34 +0200

    Test Http

--- movie_app/lib/main.dart
+++ movie_app/lib/main.dart
@@ -1,16 +1,48 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:Container()
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.orange),
+        home: MyHome());
+  }
+}
+
+class MyHome extends StatefulWidget {
+  MyHome({Key? key}) : super(key: key);
+
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Json PlaceHolder'),
+        actions: [
+          IconButton(
+              onPressed: () async {
+                Response response = await http.get(
+                    Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+                print(jsonDecode(response.body) );
+              },
+              icon: Icon(Icons.send))
+        ],
+      ),
     );
   }
 }
