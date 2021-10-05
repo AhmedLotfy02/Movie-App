c927448da1e5d42be7031bbfcb685cb61f64df0a
AhmedLotfy02 <76037906+AhmedLotfy02@users.noreply.github.com>
2021-10-05 07:32:09 +0200

    remove testing files & adding movie model

--- movie_app/lib/main.dart
+++ movie_app/lib/main.dart
@@ -1,98 +1,31 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:movie_app/controllers/appcontroller.dart';
-import 'package:provider/provider.dart';
-import 'package:http/http.dart' as http;
 
-import 'models/post.dart';
+void main() => runApp(MyMovies());
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyMovies extends StatelessWidget {
+  const MyMovies({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(primarySwatch: Colors.orange),
-        home: MyHome());
+debugShowCheckedModeBanner: false,
+theme: ThemeData(
+  primarySwatch: Colors.orange
+),
+home:Home(),
+    );
   }
 }
-
-class MyHome extends StatefulWidget {
-  MyHome({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
 
   @override
-  _MyHomeState createState() => _MyHomeState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomeState extends State<MyHome> {
-  late Future<List<Post>> futurePostsData;
-  @override
-  void initState() {
-    super.initState();
-    futurePostsData = AppController.getPosts();
-  }
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Json PlaceHolder'),
-        actions: [
-          IconButton(
-              onPressed: () async {
-                var result = await AppController.getPosts();
-                print(result);
-              },
-              icon: Icon(Icons.send))
-        ],
-      ),
-      body: FutureBuilder<List<Post>>(
-          future: futurePostsData,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      child: Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                        padding: EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius: BorderRadius.circular(15)),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '${snapshot.data![index].title}',
-                              style: TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              '${snapshot.data![index].body}',
-                              style: TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  });
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          }),
-    );
+    return Container();
   }
 }
---
+++ movie_app/lib/models/movie.dart
@@ -0,0 +1,18 @@
+class Movie {
+  late  int id;
+  late String title;
+  late double voteAverage;
+  late  String releaseDate;
+  late String overview;
+  late String posterPath;
+  Movie(this.id, this.title, this.voteAverage, this.releaseDate, this.overview,
+      this.posterPath);
+  Movie.fromJson(Map<String, dynamic> json) {
+    this.id = json['id'];
+    this.title = json['title'];
+    this.voteAverage = json['vote_average'];
+    this.releaseDate = json['release_date'];
+    this.overview = json['overview'];
+    this.posterPath = json['poster_path'];
+  }
+}
